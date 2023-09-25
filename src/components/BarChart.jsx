619cb875e722f5541a702bed5da4afb67776cf73
rehan <Technosis141@gmail.com>
2023-09-25 15:12:07 -0400

    dashboard

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -130,7 +130,6 @@
 // export default BarChart;
 
 import { useTheme } from "@mui/material";
-import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
 import { mockBarData as data } from "../data/mockData";
 import { ResponsiveBoxPlot } from "@nivo/boxplot";
